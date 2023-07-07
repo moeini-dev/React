0e59790dc12131a4bb2100967ff1d86aa3228258
moeini-dev <moeini.dev@gmail.com>
2023-07-07 18:01:55 +0330

    Design featured books items

--- src/components/FeaturedBooks/FeaturedBooks.jsx
+++ src/components/FeaturedBooks/FeaturedBooks.jsx
@@ -1,7 +1,33 @@
 import './featuredBooks.css';
+import './../../index.css';
 
 export function FeaturedBooks() {
   return (
-    <div className="container">Featured Books</div>
+    <div className="container">
+      <div className="featuredBooksTitle">Item Title</div>
+      <div className="itemWrapper">
+        <a href="google.com">
+          <div className="featuredBookItem">
+            <img className="itemImage" src="https://fakeimg.pl/800x915?font=bebas" alt="" />
+            <div className="itemName">The old man and the sea this is for text jsal</div>
+            <div className="itemAuthor">Ernest Hemingway</div>
+          </div>
+        </a>
+        <a href="google.com">
+          <div className="featuredBookItem">
+            <img className="itemImage" src="https://fakeimg.pl/800x915?font=bebas" alt="" />
+            <div className="itemName">1984</div>
+            <div className="itemAuthor">George Orwell</div>
+          </div>
+        </a>
+        <a href="google.com">
+          <div className="featuredBookItem">
+            <img className="itemImage" src="https://fakeimg.pl/800x915?font=bebas" alt="" />
+            <div className="itemName">The Master and Margarita</div>
+            <div className="itemAuthor">Mikhail Bulgakov</div>
+          </div>
+        </a>
+      </div>
+    </div>
   );
 }
