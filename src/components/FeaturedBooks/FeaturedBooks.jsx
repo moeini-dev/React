0266275466e538ffe413dd66bc252d16fb4eb6f3
moeini-dev <moeini.dev@gmail.com>
2023-07-07 11:09:09 +0330

    Change padding

---
+++ src/components/FeaturedBooks/FeaturedBooks.jsx
@@ -0,0 +1,7 @@
+import './featuredBooks.css';
+
+export function FeaturedBooks() {
+  return (
+    <div className="container">Featured Books</div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,21 @@
 import './home.css';
 import { Navbar } from '../../components/Navbar/Navbar';
+import { FeaturedBooks } from '../../components/FeaturedBooks/FeaturedBooks';
 
 window.onscroll = function () {
-    console.log(window.scrollY);
+  console.log(window.scrollY);
 }
 
 export function Home() {
-    return (
-        <div className="home">
-            <Navbar />
-            <div className="main">
-                <div className="wrapper">This is a div</div>
-            </div>
-            <div className="footer"></div>
+  return (
+    <div className="home">
+      <Navbar />
+      <div className="main">
+        <div className="wrapper">
+          <FeaturedBooks />
         </div>
-    );
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
 }
