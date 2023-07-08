2ad98660208bf770faa105c11305bcbbd6013911
moeini-dev <moeini.dev@gmail.com>
2023-07-08 15:37:02 +0330

    Create fretured books' title

--- src/components/FeaturedBooks/FeaturedBooks.jsx
+++ src/components/FeaturedBooks/FeaturedBooks.jsx
@@ -4,27 +4,30 @@ import './../../index.css';
 export function FeaturedBooks() {
   return (
     <div className="container">
-      <div className="featuredBooksTitle">Item Title</div>
+      <div className="featuredBooksTitle">
+        <div className="titleText">Top Books <br /><br />MAKE SUMMER SHINY</div>
+        <div className="offPersentage">80% OFF</div>
+      </div>
       <div className="itemWrapper">
         <a href="google.com">
           <div className="featuredBookItem">
-            <img className="itemImage" src="https://fakeimg.pl/800x915?font=bebas" alt="" />
-            <div className="itemName">The old man and the sea this is for text jsal</div>
+            <img className="itemImage" src={require("../../pics/the-old-man-and-the-sea2.jpg")} alt="" />
+            <div className="itemName">The old man and the sea</div>
             <div className="itemAuthor">Ernest Hemingway</div>
           </div>
         </a>
         <a href="google.com">
           <div className="featuredBookItem">
-            <img className="itemImage" src="https://fakeimg.pl/800x915?font=bebas" alt="" />
+            <img className="itemImage" src={require("../../pics/1984.jpg")} alt="" />
             <div className="itemName">1984</div>
             <div className="itemAuthor">George Orwell</div>
           </div>
         </a>
         <a href="google.com">
           <div className="featuredBookItem">
-            <img className="itemImage" src="https://fakeimg.pl/800x915?font=bebas" alt="" />
-            <div className="itemName">The Master and Margarita</div>
-            <div className="itemAuthor">Mikhail Bulgakov</div>
+            <img className="itemImage" src={require("../../pics/the-midnight-library.jpg")} alt="" />
+            <div className="itemName">The Midnight Library</div>
+            <div className="itemAuthor">Matt Haig</div>
           </div>
         </a>
       </div>
