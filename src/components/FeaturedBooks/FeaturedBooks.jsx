090800381e69eda9cced01a095efd20da713ba66
moeini-dev <moeini.dev@gmail.com>
2023-07-10 10:07:47 +0330

    Use item componenets

--- src/components/FeaturedBooks/FeaturedBooks.jsx
+++ src/components/FeaturedBooks/FeaturedBooks.jsx
@@ -1,5 +1,6 @@
 import './featuredBooks.css';
 import './../../index.css';
+import { Item } from './../Item/Item';
 
 export function FeaturedBooks() {
   return (
@@ -9,27 +10,23 @@ export function FeaturedBooks() {
         <div className="offPersentage">80% OFF</div>
       </div>
       <div className="itemWrapper">
-        <a href="google.com">
-          <div className="featuredBookItem">
-            <img className="itemImage" src={require("../../pics/the-old-man-and-the-sea2.jpg")} alt="" />
-            <div className="itemName">The old man and the sea</div>
-            <div className="itemAuthor">Ernest Hemingway</div>
-          </div>
-        </a>
-        <a href="google.com">
-          <div className="featuredBookItem">
-            <img className="itemImage" src={require("../../pics/1984.jpg")} alt="" />
-            <div className="itemName">1984</div>
-            <div className="itemAuthor">George Orwell</div>
-          </div>
-        </a>
-        <a href="google.com">
-          <div className="featuredBookItem">
-            <img className="itemImage" src={require("../../pics/the-midnight-library.jpg")} alt="" />
-            <div className="itemName">The Midnight Library</div>
-            <div className="itemAuthor">Matt Haig</div>
-          </div>
-        </a>
+        <Item
+          link="google.com"
+          image="the-old-man-and-the-sea2.jpg"
+          name="The old man and the sea"
+          author="Ernest Hemigway" />
+
+        <Item
+          link="google.com"
+          image="1984.jpg"
+          name="1984"
+          author="George Orwell" />
+
+        <Item
+          link="google.com"
+          image="the-midnight-library.jpg"
+          name="The Midnight Library"
+          author="Matt Haig" />
       </div>
     </div>
   );
