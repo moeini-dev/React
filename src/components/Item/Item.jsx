db2ed1f3b1dfdf140706071747b89adc958c9820
moeini-dev <moeini.dev@gmail.com>
2023-07-10 10:11:08 +0330

    Create item components

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,11 @@
+export function Item(props) {
+    return (
+        <a href={props.link}>
+            <div className="featuredBookItem">
+                <img className="itemImage" src={require(`./../../assets/${props.image}`)} alt="" />
+                <div className="itemName">{props.name}</div>
+                <div className="itemAuthor">{props.author}</div>
+            </div>
+        </a>
+    );
+}
