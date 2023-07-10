5a01a98cace9332e5795182b897c2d8279ff4ad2
moeini-dev <moeini.dev@gmail.com>
2023-07-10 18:30:28 +0330

    Change 'FeaturedBookItems' property to 'Items'

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,7 +3,7 @@ import './item.css';
 export function Item(props) {
     return (
         <a href={props.link}>
-            <div className="featuredBookItem">
+            <div className="Items">
                 <img className="itemImage" src={require(`./../../assets/${props.image}`)} alt="" />
                 <div className="itemName">{props.name}</div>
                 <div className="itemAuthor">{props.author}</div>
