bf8cc45e61e2bfe700f2601aab243149c61b0f74
moeini-dev <moeini.dev@gmail.com>
2023-07-11 15:04:21 +0330

    Add 'BookWindow' component to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,15 @@
 import './home.css';
 import { Navbar } from '../../components/Navbar/Navbar';
 import { FeaturedBooks } from '../../components/FeaturedBooks/FeaturedBooks';
-
-window.onscroll = function () {
-  console.log(window.scrollY);
-}
+import { BookWindow } from '../../components/bookWindow/BookWindow';
 
 export function Home() {
   return (
     <div className="home">
       <Navbar />
       <div className="main">
-        <div className="wrapper">
-          <FeaturedBooks />
-        </div>
+        <FeaturedBooks />
+        <BookWindow />
       </div>
       <div className="footer"></div>
     </div>
