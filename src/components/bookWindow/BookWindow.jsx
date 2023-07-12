59f59164d8440ced713090f9c57f5c64cb21eb36
moeini-dev <moeini.dev@gmail.com>
2023-07-12 09:15:45 +0330

    Fix position and transition of arrows

--- src/components/bookWindow/BookWindow.jsx
+++ src/components/bookWindow/BookWindow.jsx
@@ -75,7 +75,7 @@ export function BookWindow() {
           author="Matt Haig" />
       </div>
       <div className="arrowRight">
-        <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m375-240-43-43 198-198-198-198 43-43 241 241-241 241Z" /></svg>
+        <svg className="arrowRightIcon" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m375-240-43-43 198-198-198-198 43-43 241 241-241 241Z" /></svg>
       </div>
     </div >
   );
