ac5d2fb9e48d5c1f0944985acfc304327a36e94c
moeini-dev <moeini.dev@gmail.com>
2023-07-13 15:12:16 +0330

    Put page 'Login' in 'app.js' just for demostraition purposes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Home } from './pages/home/Home';
 import { Register } from './pages/register/Register';
+import { Login } from './pages/login/Login';
 
 function App() {
   return (
     <>
       {/* <Home /> */}
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </>
   );
 }
