ba60ee522e699e4d7adb9f3b310ee43717d98e99
moeini-dev <moeini.dev@gmail.com>
2023-07-13 14:45:26 +0330

    Put page 'Register' in 'app.js' just for demostraition purposes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Home } from './pages/home/Home';
+import { Register } from './pages/register/Register';
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Register />
     </>
   );
 }
