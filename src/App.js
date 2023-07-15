c2479b3c7f951e8f2f0ad452783fdf55203825b5
moeini-dev <moeini.dev@gmail.com>
2023-07-15 20:52:27 +0330

    Add 'Book' component just for demonstration purposes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { Home } from './pages/home/Home';
 import { Register } from './pages/register/Register';
 import { Login } from './pages/login/Login';
+import { Book } from './pages/book/Book';
 
 function App() {
   return (
     <>
       {/* <Home /> */}
       {/* <Register /> */}
-      <Login />
+      {/* <Login /> */}
+      <Book
+        image="the-old-man-and-the-sea2.jpg" />
     </>
   );
 }
