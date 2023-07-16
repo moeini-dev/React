e6f762361d28c1d0d61930708f71d098a622f2c3
moeini-dev <moeini.dev@gmail.com>
2023-07-16 10:49:23 +0330

    Add description portion

--- src/pages/book/Book.jsx
+++ src/pages/book/Book.jsx
@@ -35,8 +35,20 @@ export function Book(props) {
             <button className="addToCart">Add to your cart</button>
           </div>
         </div>
-        <div className="bookDescription"></div>
+        <div className="bookDescription">
+          <h2 className="bookDescriptionHeader">"The old man and the sea" by Ernest Hemingway</h2>
+          <div className="bookDescriptionMain">
+            <article>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consequatur totam blanditiis, ex commodi laudantium natus et aut nisi molestias tempore harum voluptatum excepturi? Impedit asperiores repellendus  cumque modi aperiam. <br /> <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptas aliquid temporibus cum consequuntur dolores incidunt dolore cupiditate repudiandae, unde deleniti assumenda maxime veniam facere ipsum ipsam aspernatur quaerat ex? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus mollitia laudantium praesentium possimus ipsum, veniam ullam saepe eaque quae ipsa quos asperiores voluptatem commodi quas optio ex dolore dolorem vel.lo
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.<br /> Voluptas non explicabo voluptatum eveniet. Earum quia necessitatibus dicta? Accusamus aliquid expedita incidunt fugit est facilis dolorem repudiandae quidem nostrum! Fugit, placeat?<br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas corporis incidunt enim est accusantium?<br /> Et repellendus, fugiat minus labore beatae omnis? Voluptatibus temporibus consectetur porro. Quis voluptate facere unde natus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eveniet modi obcaecati odit impedit excepturi quod fugiat ea eaque sint quos recusandae autem inventore vitae ipsam, veniam facilis, omnis earum!
+            </article>
+          </div>
+        </div>
+        <div className="bookComments">
+
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
