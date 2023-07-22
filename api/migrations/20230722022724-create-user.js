a6d4272ee7cb5b0694a28ab2ea905521c3bfba04
moeini-dev <moeini.dev@gmail.com>
2023-07-22 10:11:30 +0330

    Create 'User' model and its migration

---
+++ api/migrations/20230722022724-create-user.js
@@ -0,0 +1,61 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      uuid: {
+        type: DataTypes.UUID,
+        allownull: false,
+        defaultValue: DataTypes.UUIDV4
+      },
+      username: {
+        type: DataTypes.STRING,
+        allownull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allownull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allownull: false,
+        validate: {
+          isEmail: {
+            msg: 'Enter a valid email'
+          }
+        }
+      },
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allownull: false,
+        defaultValue: 0,
+        validate: {
+          isIn: {
+            args: [[true, false]],
+            msg: 'Invalid input: Use 1 for Admin -- 0 for Customer'
+          }
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.DATE
+      }
+    },
+      { freezeTableName: true });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable('user');
+  }
+};
---
+++ api/models/user.js
@@ -0,0 +1,73 @@
+// const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('./index');
+require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   { dialect: 'mysql' }
+// );
+
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   User.init({
+//     uuid: DataTypes.UUID,
+//     username: DataTypes.STRING,
+//     password: DataTypes.STRING,
+//     email: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'User',
+//   });
+//   return User;
+// };
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    uuid: {
+      type: DataTypes.UUID,
+      allownull: false,
+      defaultValue: DataTypes.UUIDV4
+    },
+    username: {
+      type: DataTypes.STRING,
+      allownull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allownull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allownull: false,
+      validate: {
+        isEmail: {
+          msg: 'Enter a valid email'
+        }
+      }
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allownull: false,
+      defaultValue: 0,
+      validate: {
+        isIn: {
+          args: [[true, false]],
+          msg: 'Invalid input: Use 1 for Admin -- 0 for Customer'
+        }
+      }
+    }
+  },
+    { freezeTableName: true })
+  return User;
+}
