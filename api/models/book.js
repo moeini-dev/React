71d39980b8ea5e4be04312a355773da64db348f9
moeini-dev <moeini.dev@gmail.com>
2023-07-25 12:49:00 +0330

    Create book model

---
+++ api/models/book.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define('book', {
+    isbn: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      unique: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    publicationYear: {
+      type: DataTypes.INTEGER
+    },
+    price: {
+      type: DataTypes.FLOAT(5, 2),
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, { freezeTableName: true })
+  return Book;
+}
