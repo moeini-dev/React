bb5641537044055b0739ba1d105910212e321e7a
moeini-dev <moeini.dev@gmail.com>
2023-08-17 20:33:39 +0330

    Add 'deleteBook' and 'addBook' to book controller

--- api/routes/book.js
+++ api/routes/book.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bookControllers = require('../controllers/bookController');
 
 router.get('/', bookControllers.getBooks);
-
+router.delete('/:isbn', bookControllers.deleteBook);
+router.post('/', bookControllers.addBook);
 
 module.exports = router;
