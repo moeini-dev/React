b24e5a442c3425fd15f6b08b73374b8535cf0721
moeini-dev <moeini.dev@gmail.com>
2023-08-22 21:56:22 +0330

    Add routes to the server.js file to use routes

--- api/server.js
+++ api/server.js
@@ -3,7 +3,16 @@ const app = express();
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 const db = require('./models');
+const bcrypt = require('bcrypt');
 app.use(express.json());
+
+const userRoute = require('./routes/user');
+const bookRoute = require('./routes/book');
+const authRoute = require('./routes/auth');
+
+app.use('/user', userRoute);
+app.use('/book', bookRoute);
+app.use('/auth', authRoute);
 
 // app.get("/api/featuredBooks", (req, res) => {
 //     res.json(
@@ -16,29 +25,35 @@ app.use(express.json());
 //     );
 // })
 
-app.get('/api/book', async (req, res) => {
-    await db.book.create({
-        title: "testBook",
-        isbn: 9772848246584,
-        price: 9.5,
-        publicationYear: 2014
-    }).then(result => console.log('~~ Done! ~~'))
-        .catch(err => console.log(err))
-    res.json({
-        msg: 'This is just for testing books {JSON}'
-    })
+// app.get('/api/review', async (req, res) => {
+//     await db.review.create({
+//         userUuid: "38b8805e-9c3f-45ab-89a6-1295ef2381ce",
+//         bookIsbn: 976465416584,
+//         reviewText: "Nice!"
+//     }).then(result => console.log('~~ Done! ~~'))
+//         .catch(err => console.log(err))
+//     res.json({
+//         msg: 'This is just for testing reviews {JSON}'
+//     })
+// })
+
+
+app.get('/api/user', async (req, res) => {
+  // bcrypt.hash(plainTextPass, 8)
+  //   .then(result => console.log('Done: ', result))
+  //   .catch(err => console.log('Error: ', err))
+
 })
 
 
 
-
 app.listen(5000, async () => {
-    try {
-        console.log('~ Server is up and running on http://localhost:5000');
-        await db.sequelize.sync()
-            .then(result => console.log('~ Database synced'))
-            .catch(err => console.log('[ERROR] - Something went wrong with the database'))
-    } catch (err) {
-        console.log('[ERROR] - Something went wrong with the server', err)
-    }
+  try {
+    console.log('~ Server is up and running on http://localhost:5000');
+    await db.sequelize.sync()
+      .then(result => console.log('~ Database synced'))
+      .catch(err => console.log('[ERROR] - Something went wrong with the database', err))
+  } catch (err) {
+    console.log('[ERROR] - Something went wrong with the server', err)
+  }
 });
