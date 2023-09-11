34793a9e538d4fe988976ad648061aeca0a28e09
moeini-dev <moeini.dev@gmail.com>
2023-09-11 15:14:24 +0330

    Edit authContext files for being able to handle errors to

--- react_project/src/authContext/apiCall.js
+++ react_project/src/authContext/apiCall.js
@@ -10,11 +10,12 @@ export const login = async (user, dispatch) => {
                 'Content-Type': 'application/json'
             }
         })
-        const userData = res.data
+        const userData = res.data;
         console.log('Data from apiCall: ', res)
         dispatch({ type: 'loggin_seccess', user: userData })
     } catch (err) {
-        dispatch({ type: 'loggin_failure' })
-        console.log('Error from apiCall: ', err)
+        const errorData = err.response.data.msg;
+        dispatch({ type: 'loggin_failure', error: errorData })
+        console.log('Error from apiCall: ')
     }
 }
