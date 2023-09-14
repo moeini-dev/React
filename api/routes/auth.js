2e6ec623f79ecf02424cf0731ac4927a04fb84c1
moeini-dev <moeini.dev@gmail.com>
2023-09-14 06:37:58 +0330

    Add Logout fuctionality

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authControllers = require('../controllers/authController');
 
 router.post('/login', authControllers.login);
+router.get('/logout', authControllers.logout);
 router.post('/refresh', authControllers.verify, authControllers.refresh);
 
 
